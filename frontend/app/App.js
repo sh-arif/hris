01bd32701ec7c5a1e00a602b1c58e47d3c622c65
DanielPhyo <phyopyaethein14@gmail.com>
2021-08-17 20:23:46 +0800

    Add files via upload
    Phyo Update 17 Aug

---
+++ frontend/app/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LeavesScreen from './App/screens/LeavesScreen';
+
+import TimeScreen from './App/screens/TimeScreen';
+import WelcomeScreen from './App/screens/WelcomeScreen';
+
+
+
+export default function App() {
+  return (
+
+    //Home Screen
+    //<WelcomeScreen />
+    //<TimeScreen />
+    <LeavesScreen />
+
+
+
+
+  );
+}
+
+
