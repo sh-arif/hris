16e5a5e75033fc3b639f606298c9e49d8255455e
Kashif Aftab <kashifaftab07@gmail.com>
2021-08-18 19:11:59 +0530

    change1

--- backend/src/main/java/com/hris/employee/dao/entities/Salary.java
+++ backend/src/main/java/com/hris/employee/dao/entities/Salary.java
@@ -1,2 +1,10 @@
-package com.hris.employee.dao.entities;public class Salary {
+package com.hris.employee.dao.models;
+
+public class Salary {
+    public static void main(int houseallowance,int caballowance,int bonus,int hoursworked,int overtime)
+    {
+        double Salary=houseallowance+caballowance+bonus*overtime+hoursworked;
+        System.out.println(Salary);
+
+    }
 }
