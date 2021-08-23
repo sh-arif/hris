545f17722e4a4f3e7538da74d21c3bb7353e9d44
Kashif Aftab <kashifaftab07@gmail.com>
2021-08-23 20:17:11 +0530

    "changes"

---
+++ backend/src/main/java/com/hris/employee/dao/entities/AttendanceforMonth.java
@@ -0,0 +1,27 @@
+package com.hris.employee.dao.entities;
+import java.util.*;
+
+public class AttendanceforMonth
+{
+    public static void main(String args[])
+    {   Scanner sc=new Scanner();
+        String namenattendance [][]=new String [30][30];
+        chekin check out date
+        String ta;
+        String tn;
+        for(int i=0;i<30;i++)
+        {
+            for(int j=0;j<30;j++)
+            {
+                System.out.println("Enter the name and attendance of day 1");
+                tn=sc.nextLine();
+                ta=sc.nextLine()
+                namenattendance[i][i]=tn;
+                namenattendance[i][j]=ta;
+            }
+        }
+        {
+
+        }
+        }
+}
